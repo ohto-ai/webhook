db7fa0d7eab0ede4a9ad54fe385bbdd5c918fd08
Ohto-Ai <zhu.thatboy@outlook.com>
2022-09-02 03:12:23 +0800

    Add userlogin for GET
    Add brynet lib

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 #include <fstream>
+#include <algorithm>
+#include <iterator>
+#include <brynet/base/crypto/Base64.hpp>
 
 #ifdef _WIN32
 #define popen _popen
@@ -27,6 +30,43 @@ std::string executeCommand(std::string cmd)
 	return display;
 }
 
+namespace ohtoai
+{
+	namespace tool
+	{
+		namespace string
+		{
+			std::vector<std::string> split(const std::string& s, const std::string& delimiters)
+			{
+				std::vector<std::string> tokens;
+				std::string::size_type lastPos = s.find_last_not_of(delimiters, 0);
+				std::string::size_type pos = s.find_first_of(delimiters, lastPos);
+				while (std::string::npos != pos || std::string::npos != lastPos)
+				{
+					tokens.push_back(s.substr(lastPos, pos - lastPos));
+					lastPos = s.find_first_not_of(delimiters, pos);
+					pos = s.find_first_of(delimiters, lastPos);
+				}
+				return tokens;
+			}
+
+			std::string& trim(std::string& s)
+			{
+				if (s.empty())
+					return s;
+
+				s.erase(0, s.find_first_not_of(" "));
+				s.erase(s.find_last_not_of(" ") + 1);
+				return s;
+			}
+		}
+	}
+}
+
+struct UserLogin {
+	std::string username;
+	std::string password;
+};
 
 int main()
 {
@@ -52,11 +92,48 @@ int main()
 
 	httplib::Server server;
 
+	UserLogin globalUserLogin{ j["auth"]["username"], j["auth"]["password"] };
+	bool isAuthEnabled = j["auth"]["enabled"];
+	
+
 	server.bind_to_port(j["server"]["host"].get<std::string>().c_str(), j["server"]["port"]);
 
-	server.set_pre_routing_handler([&server](const httplib::Request& req, httplib::Response& res)
+	server.set_pre_routing_handler([&server, &globalUserLogin, &isAuthEnabled](const httplib::Request& req, httplib::Response& res)
 		{
 			spdlog::info("Routing {} {} \nReceive {} bytes\n{}", req.method, req.path, req.body.size(), req.body);
+
+			if (isAuthEnabled)
+			{
+				if (req.method == "GET")
+				{
+					if (!req.has_header("Authorization") || req.get_header_value("Authorization").empty())
+					{
+						res.set_header("WWW-Authenticate", R"(Basic realm="Secure Area")");
+						res.status = 401;
+						return httplib::Server::HandlerResponse::Handled;
+					}
+					auto basic_auth_base64 = ohtoai::tool::string::split(req.get_header_value("Authorization"), " ").back();
+					auto basic_auth = brynet::base::crypto::base64_decode(basic_auth_base64);
+					auto auth = ohtoai::tool::string::split(basic_auth, ":");
+					if (auth.size() != 2)
+					{
+						spdlog::error("Error base64: {}", basic_auth);
+						res.set_header("WWW-Authenticate", R"(Basic realm="Secure Area")");
+						res.status = 401;
+						return httplib::Server::HandlerResponse::Handled;
+					}
+
+					if (auth[0] != globalUserLogin.username || auth[1] != globalUserLogin.password)
+					{
+						res.set_header("WWW-Authenticate", R"(Basic realm="Secure Area")");
+						res.status = 401;
+						return httplib::Server::HandlerResponse::Handled;
+					}
+
+					spdlog::info("Auth {}", auth[0]);
+				}
+			}
+			
 			return httplib::Server::HandlerResponse::Unhandled;
 		});
 
