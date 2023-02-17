0d3f413c35d6d04d38bbfaf9214497eac1aa7249
OhtoAi <ohtoai.choo@gmail.com>
2023-02-18 02:22:24 +0800

    Feat/make content multiline (#37)
    * Enable content multiline
    
    * Remove single line in content
    
    * Update version to v0.0.3

--- src/config_modal.hpp
+++ src/config_modal.hpp
@@ -1,20 +1,23 @@
 #pragma once
 #include <nlohmann/json.hpp>
 
-struct BasicAuth {
+struct BasicAuth
+{
     std::string username = "";
     std::string password = "";
     std::string path = "/";
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(BasicAuth, username, password, path)
 };
 
-struct Result {
+struct Result
+{
     std::string type = "text/plain";
-    std::string content = "";
+    std::vector<std::string> content = {};
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(Result, type, content)
 };
 
-struct Hook {
+struct Hook
+{
     std::string command = "";
     std::string method = "GET";
     std::string name = "";
@@ -23,7 +26,8 @@ struct Hook {
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(Hook, command, method, name, path, result)
 };
 
-struct Listen {
+struct Listen
+{
     BasicAuth auth;
     std::string host = "localhost";
     int port = 8080;
@@ -31,7 +35,8 @@ struct Listen {
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(Listen, auth, host, port, prefix)
 };
 
-struct Log {
+struct Log
+{
     std::string console_level = "info";
     std::string file_level = "info";
     std::string file_path = "webhook.log";
@@ -39,24 +44,30 @@ struct Log {
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(Log, console_level, file_level, file_path, global_level)
 };
 
-struct WebhookConfigModal {
+struct WebhookConfigModal
+{
     std::vector<Hook> hooks;
     Listen listen;
     Log log;
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(WebhookConfigModal, hooks, listen, log)
 
-    static WebhookConfigModal load(const std::string &configPath) {
+    static WebhookConfigModal load(const std::string &configPath)
+    {
         std::ifstream ifs(configPath);
-        if (!ifs.is_open()) {
+        if (!ifs.is_open())
+        {
             throw std::runtime_error(fmt::format("Failed to open config file: {}", configPath));
         }
 
         WebhookConfigModal config;
-        try {
+        try
+        {
             nlohmann::json configJ;
             ifs >> configJ;
             configJ.get_to(config);
-        } catch (std::exception e) {
+        }
+        catch (std::exception e)
+        {
             throw std::runtime_error(fmt::format("Failed to parse config file: {}", e.what()));
         }
 
@@ -64,14 +75,16 @@ struct WebhookConfigModal {
         return config;
     }
 
-    static void save(const std::string &configPath, const WebhookConfigModal &config) {
+    static void save(const std::string &configPath, const WebhookConfigModal &config)
+    {
         nlohmann::json configJ = config;
         std::ofstream ofs(configPath);
         ofs << configJ.dump(4);
         ofs.close();
     }
 
-    static void generate(const std::string &configPath) {
+    static void generate(const std::string &configPath)
+    {
         WebhookConfigModal config;
         Hook demoHook = {
             .command = "echo -n \"Hello\"",
@@ -80,13 +93,15 @@ struct WebhookConfigModal {
             .path = "/hi",
             .result = {
                 .type = "text/html",
-                .content = R"(<h1>{{#response}}{{&command_output}}{{/response}} {{&app}} {{&version}}</h1>
-{{#request}}
-<p>Method: {{&method}}</p>
-<p>Path: {{&path}}</p>
-<p>User-Agent: {{&user_agent}}</p>
-<p>Client: {{&remote_addr}}:{{&remote_port}}</p>
-{{/request}})",
+                .content = {
+                    "<h1>{{#response}}{{&command_output}}{{/response}} {{&app}} {{&version}}</h1>",
+                    "{{#request}}",
+                    "<p>Method: {{&method}}</p>",
+                    "<p>Path: {{&path}}</p>",
+                    "<p>User-Agent: {{&user_agent}}</p>",
+                    "<p>Client: {{&remote_addr}}:{{&remote_port}}</p>",
+                    "{{/request}}",
+                },
             },
         };
         config.hooks.push_back(demoHook);
