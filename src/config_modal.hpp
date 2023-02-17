e05573127188a4e3ab93e2039130f943bdb54b28
OhtoAi <zhuguangfeng.thatboy@gmail.com>
2023-02-17 23:53:57 +0800

    Fix action/setup-cpp (#26)
    * Fix action/setup-cpp
    
    * Add action debug
    
    * Modify action debug
    
    * Fix action and Move inc
    
    * Fix blank

--- src/config_modal.hpp
+++ src/config_modal.hpp
@@ -44,4 +44,46 @@ struct WebhookConfigModal {
     Listen listen;
     Log log;
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(WebhookConfigModal, hooks, listen, log)
+
+    static WebhookConfigModal load(const std::string &configPath) {
+        std::ifstream ifs(configPath);
+        if (!ifs.is_open()) {
+            throw std::runtime_error(fmt::format("Failed to open config file: {}", configPath));
+        }
+
+        WebhookConfigModal config;
+        try {
+            nlohmann::json configJ;
+            ifs >> configJ;
+            configJ.get_to(config);
+        } catch (std::exception e) {
+            throw std::runtime_error(fmt::format("Failed to parse config file: {}", e.what()));
+        }
+
+        ifs.close();
+        return config;
+    }
+
+    static void save(const std::string &configPath, const WebhookConfigModal &config) {
+        nlohmann::json configJ = config;
+        std::ofstream ofs(configPath);
+        ofs << configJ.dump(4);
+        ofs.close();
+    }
+
+    static void generate(const std::string &configPath) {
+        WebhookConfigModal config;
+        Hook demoHook = {
+            .command = "echo -n \"Hello\"",
+            .method = "GET",
+            .name = "hi",
+            .path = "/hi",
+            .result = {
+                .type = "text/html",
+                .content = "<h1>{{&command_output}} {{&app}}</h1>",
+            },
+        };
+        config.hooks.push_back(demoHook);
+        save(configPath, config);
+    }
 };
