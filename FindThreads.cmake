9e37d088edf033b0e22a72d616d9ead8618aed70
请问墙和柱子能奶你吗 <ohtoai.choo@gmail.com>
2023-03-17 17:24:50 +0800

    Delete FindThreads.cmake

--- FindThreads.cmake
+++
@@ -1,8 +0,0 @@
-# add pthread support
-find_package (Threads REQUIRED)
-if (THREADS_HAVE_PTHREAD_ARG)
-  target_compile_options (${PROJECT_NAME} PUBLIC "-pthread")
-endif ()
-if (CMAKE_THREAD_LIBS_INIT)
-  target_link_libraries (${PROJECT_NAME} "${CMAKE_THREAD_LIBS_INIT}")
-endif ()
