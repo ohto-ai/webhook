14589acc750073fa36a4efff1d789e0905da6bae
请问墙和柱子能奶你吗 <zhu.thatboy@outlook.com>
2023-03-21 16:56:53 +0800

    Remove Windows define in config_modal

--- src/config/config_modal.hpp
+++ src/config/config_modal.hpp
@@ -1,14 +1,5 @@
 #pragma once
 
-#ifdef _WIN32
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-#endif
-
 #include <nlohmann/json.hpp>
 
 struct BasicAuth
