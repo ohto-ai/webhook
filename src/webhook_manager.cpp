3a92b7cbbbf99330ea156bb4bf540c778b84c397
请问墙和柱子能奶你吗 <zhu.thatboy@outlook.com>
2023-03-31 11:51:03 +0800

    Split func and data

--- src/webhook_manager.cpp
+++ src/webhook_manager.cpp
@@ -39,42 +39,7 @@ int ohtoai::WebhookManager::exec()
                           }
                           fmt::print(fg(fmt::color::green), "\r{:=^{}}\n", " Done ", PlatformHelper::getInstance().getTerminalWidth()); });
 
-    server.set_pre_routing_handler([this](const httplib::Request &req, httplib::Response &res)
-                                   {
-                                       if (req.has_header("X-Real-IP"))
-                                       {
-                                           const_cast<httplib::Request &>(req).remote_addr = req.get_header_value("X-Real-IP");
-                                       }
-
-                                       if (!config.listen.auth.path.empty() && !config.listen.auth.username.empty() && !config.listen.auth.password.empty() && fplus::is_prefix_of(config.listen.auth.path, req.path))
-                                       {
-                                           // verify username && password
-                                           if (req.has_header("Authorization"))
-                                           {
-                                               std::string auth = req.get_header_value("Authorization");
-                                               if (auth.find("Basic ") == 0)
-                                               {
-                                                   auth = auth.substr(6);
-                                                   auth = base64::encode(auth);
-                                                   if (auth.find(":") != std::string::npos)
-                                                   {
-                                                       std::string username = auth.substr(0, auth.find(":"));
-                                                       std::string password = auth.substr(auth.find(":") + 1);
-                                                       if (username == config.listen.auth.username && password == config.listen.auth.password)
-                                                       {
-                                                           return httplib::Server::HandlerResponse::Unhandled;
-                                                       }
-                                                   }
-                                               }
-                                           }
-
-                                           spdlog::warn("Auth failed");
-                                           res.status = 401;
-                                           res.set_header("WWW-Authenticate", "Basic realm=\"Webhook\"");
-                                           res.set_content("Unauthorized", "text/plain");
-                                           return httplib::Server::HandlerResponse::Handled;
-                                       }
-                                       return httplib::Server::HandlerResponse::Unhandled; });
+    server.set_pre_routing_handler([this](const auto& req, auto& res) { return authRoutingHandler(req, res);});
 
     spdlog::info("Server listen {}:{}.", config.listen.host, config.listen.port);
 
@@ -162,6 +127,149 @@ bool ohtoai::WebhookManager::installLoggers()
 bool ohtoai::WebhookManager::installHooks()
 {
     using nlohmann::literals::operator"" _json_pointer;
+
+    for (const auto &hook : config.hooks)
+    {
+        std::string name = hook.name;
+        std::string method = fplus::to_upper_case(hook.method);
+        std::string path = fmt::format("{}{}", config.listen.prefix, hook.path);
+        std::string command = hook.command;
+        std::string content_type = hook.result.type;
+        std::string content = fmt::format("{}", fmt::join(hook.result.content, "\n"));
+        int command_timeout = hook.command_timeout;
+
+        spdlog::info("Bind `{}` {} {} hook, with command `{}`", name, method, path, command);
+
+        auto handler = [=](const httplib::Request &req, httplib::Response &res)
+        {
+            spdlog::info("Trigger hook `{}`", name);
+
+            auto env = injaEnv;
+            auto data = basic_render_data;
+
+            data["/context/name"_json_pointer] = name;
+            data["/context/command"_json_pointer] = command;
+            data["/request/method"_json_pointer] = req.method;
+            data["/request/path"_json_pointer] = req.path;
+            data["/request/body"_json_pointer] = req.body;
+            data["/request/remote_addr"_json_pointer] = req.remote_addr;
+            data["/request/remote_port"_json_pointer] = req.remote_port;
+            for (const auto &[key, value] : req.headers)
+                data["/request/header"_json_pointer][key] = value;
+            data["/request/content_length"_json_pointer] = req.body.size();
+            auto rendered_command = env.render(command, data);
+            data["/context/rendered_command"_json_pointer] = rendered_command;
+
+            auto command_output_future = PlatformHelper::getInstance().executeCommandAsync(rendered_command);
+            env.add_callback("command_output", 0, [&command_output_future, command_timeout](inja::Arguments &args) -> nlohmann::json
+                {
+                if (command_timeout > 0)
+                {
+                    if(command_output_future.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready)
+                    {
+                        spdlog::info("Waiting for command output... (timeout: {}ms)", command_timeout);
+                    }
+                    if (command_output_future.wait_for(std::chrono::milliseconds(command_timeout)) == std::future_status::ready)
+                    {
+                        spdlog::info("Command output received");
+                        return command_output_future.get();
+                    }
+                    else
+                    {
+                        spdlog::warn("Command output timeout");
+                        return std::string{};
+                    }
+                }
+                else
+                {
+                    spdlog::info("Waiting for command output...");
+                    return command_output_future.get();
+                } });
+            // add try-catch
+            try
+            {
+                auto result = env.render(content, data);
+                res.set_content(result, content_type.c_str());
+                if (result.size() > 1024)
+                    spdlog::debug("Render: \n\r{}...\n", result.substr(0, 1024));
+                else
+                    spdlog::debug("Render: \n\r{}\n", result);
+            }
+            catch (const std::exception &e)
+            {
+                spdlog::error("Render content failed: {}", e.what());
+                res.status = 500;
+                res.set_content("Render content failed", "text/plain");
+                return;
+            }
+        };
+        if (method == "GET")
+            server.Get(path.c_str(), handler);
+        else if (method == "POST")
+            server.Post(path.c_str(), handler);
+        else if (method == "Delete")
+            server.Delete(path.c_str(), handler);
+        else if (method == "Put")
+            server.Put(path.c_str(), handler);
+        else if (method == "Options")
+            server.Options(path.c_str(), handler);
+        else if (method == "Patch")
+            server.Patch(path.c_str(), handler);
+        else
+            spdlog::error("Illegal method: {}", method);
+    }
+
+    return true;
+}
+
+httplib::Server::HandlerResponse authRoutingHandler(const httplib::Request &req, httplib::Response &res)
+{
+    if (req.has_header("X-Real-IP"))
+    {
+        const_cast<httplib::Request &>(req).remote_addr = req.get_header_value("X-Real-IP");
+    }
+
+    if (!config.listen.auth.path.empty() && !config.listen.auth.username.empty() && !config.listen.auth.password.empty() && fplus::is_prefix_of(config.listen.auth.path, req.path))
+    {
+        // verify username && password
+        if (req.has_header("Authorization"))
+        {
+            std::string auth = req.get_header_value("Authorization");
+            if (auth.find("Basic ") == 0)
+            {
+                auth = auth.substr(6);
+                auth = base64::encode(auth);
+                if (auth.find(":") != std::string::npos)
+                {
+                    std::string username = auth.substr(0, auth.find(":"));
+                    std::string password = auth.substr(auth.find(":") + 1);
+                    if (username == config.listen.auth.username && password == config.listen.auth.password)
+                    {
+                        return httplib::Server::HandlerResponse::Unhandled;
+                    }
+                }
+            }
+        }
+
+        spdlog::warn("Auth failed");
+        res.status = 401;
+        res.set_header("WWW-Authenticate", "Basic realm=\"Webhook\"");
+        res.set_content("Unauthorized", "text/plain");
+        return httplib::Server::HandlerResponse::Handled;
+    }
+    return httplib::Server::HandlerResponse::Unhandled; 
+}
+
+ohtoai::WebhookManager::WebhookManager(int argc, char **argv) : configurator(ghc::filesystem::path(PlatformHelper::getInstance().getProgramDirectory()) / "hook.json") {
+    ghc::filesystem::current_path(PlatformHelper::getInstance().getProgramDirectory());
+    basic_render_data["/context/app"_json_pointer] = VersionHelper::getInstance().AppName;
+    basic_render_data["/context/version"_json_pointer] = VersionHelper::getInstance().Version;
+    basic_render_data["/context/commit_hash"_json_pointer] = VersionHelper::getInstance().CommitHash;
+    basic_render_data["/context/commit_date"_json_pointer] = VersionHelper::getInstance().CommitDate;
+    basic_render_data["/context/build_date"_json_pointer] = VersionHelper::getInstance().BuildDate;
+    basic_render_data["/context/build_time"_json_pointer] = VersionHelper::getInstance().BuildTime;
+    basic_render_data["/context/platform"_json_pointer] = PlatformHelper::getInstance().getPlatform();
+
     injaEnv.add_void_callback("info", [](inja::Arguments &args)
                               {
         std::vector<std::string> result(args.size());
@@ -181,128 +289,6 @@ bool ohtoai::WebhookManager::installHooks()
         std::transform(args.begin(), args.end(), result.begin(),
                 [](const nlohmann::json* j) { return j->get<std::string>(); });
         spdlog::error("{}", fmt::join(result, " ")); });
-
-    for (const auto &hook : config.hooks)
-    {
-        std::string name = hook.name;
-        std::string method = fplus::to_upper_case(hook.method);
-        std::string path = fmt::format("{}{}", config.listen.prefix, hook.path);
-        std::string command = hook.command;
-        std::string content_type = hook.result.type;
-        std::string content = fmt::format("{}", fmt::join(hook.result.content, "\n"));
-        int command_timeout = hook.command_timeout;
-
-        spdlog::info("Bind `{}` {} {} hook, with command `{}`", name, method, path, command);
-
-        auto handler = [=](const httplib::Request &req, httplib::Response &res)
-        {
-            spdlog::info("Trigger hook `{}`", name);
-
-            auto env = injaEnv;
-            nlohmann::json data;
-
-            data["/context/name"_json_pointer] = name;
-            data["/context/command"_json_pointer] = command;
-            data["/context/app"_json_pointer] = VersionHelper::getInstance().AppName;
-            data["/context/version"_json_pointer] = VersionHelper::getInstance().Version;
-            data["/context/commit_hash"_json_pointer] = VersionHelper::getInstance().CommitHash;
-            data["/context/commit_date"_json_pointer] = VersionHelper::getInstance().CommitDate;
-            data["/context/build_date"_json_pointer] = VersionHelper::getInstance().BuildDate;
-            data["/context/build_time"_json_pointer] = VersionHelper::getInstance().BuildTime;
-            data["/context/platform"_json_pointer] = PlatformHelper::getInstance().getPlatform();
-            data["/request/method"_json_pointer] = req.method;
-            data["/request/path"_json_pointer] = req.path;
-            data["/request/body"_json_pointer] = req.body;
-            data["/request/remote_addr"_json_pointer] = req.remote_addr;
-            data["/request/remote_port"_json_pointer] = req.remote_port;
-            for (const auto &[key, value] : req.headers)
-                data["/request/header"_json_pointer][key] = value;
-            data["/request/content_length"_json_pointer] = req.body.size();
-            auto rendered_command = env.render(command, data);
-            data["/context/rendered_command"_json_pointer] = rendered_command;
-
-            auto command_output_future = PlatformHelper::getInstance().executeCommandAsync(rendered_command);
-            env.add_callback("command_output", 0, [&command_output_future, command_timeout](inja::Arguments &args) -> nlohmann::json
-                             {
-                                if (command_timeout > 0)
-                                {
-                                    if(command_output_future.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready)
-                                    {
-                                        spdlog::info("Waiting for command output... (timeout: {}ms)", command_timeout);
-                                    }
-                                    if (command_output_future.wait_for(std::chrono::milliseconds(command_timeout)) == std::future_status::ready)
-                                    {
-                                        spdlog::info("Command output received");
-                                        return command_output_future.get();
-                                    }
-                                    else
-                                    {
-                                        spdlog::warn("Command output timeout");
-                                        return std::string{};
-                                    }
-                                }
-                                else
-                                {
-                                    spdlog::info("Waiting for command output...");
-                                    return command_output_future.get();
-                                } });
-            // add try-catch
-            try
-            {
-                auto result = env.render(content, data);
-                res.set_content(result, content_type.c_str());
-                if (result.size() > 1024)
-                {
-                    spdlog::debug("Render: \n\r{}...\n", result.substr(0, 1024));
-                }
-                else
-                {
-                    spdlog::debug("Render: \n\r{}\n", result);
-                }
-            }
-            catch (const std::exception &e)
-            {
-                spdlog::error("Render content failed: {}", e.what());
-                res.status = 500;
-                res.set_content("Render content failed", "text/plain");
-                return;
-            }
-        };
-        if (method == "GET")
-        {
-            server.Get(path.c_str(), handler);
-        }
-        else if (method == "POST")
-        {
-            server.Post(path.c_str(), handler);
-        }
-        else if (method == "Delete")
-        {
-            server.Delete(path.c_str(), handler);
-        }
-        else if (method == "Put")
-        {
-            server.Put(path.c_str(), handler);
-        }
-        else if (method == "Options")
-        {
-            server.Options(path.c_str(), handler);
-        }
-        else if (method == "Patch")
-        {
-            server.Patch(path.c_str(), handler);
-        }
-        else
-        {
-            spdlog::error("Illegal method: {}", method);
-        }
-    }
-
-    return true;
-}
-
-ohtoai::WebhookManager::WebhookManager(int argc, char **argv) : configurator(ghc::filesystem::path(PlatformHelper::getInstance().getProgramDirectory()) / "hook.json") {
-    ghc::filesystem::current_path(PlatformHelper::getInstance().getProgramDirectory());
 }
 
 ohtoai::WebhookManager::~WebhookManager() {}
