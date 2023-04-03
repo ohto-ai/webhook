d955d09ffae272141523f9bb7041d6b0b1f91b8a
请问墙和柱子能奶你吗 <ohtoai.choo@gmail.com>
2023-04-04 02:56:41 +0800

    Add default icon (#97)

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,23 @@ int main(int argc, char **argv)
 {
     while(true) {
         ohtoai::WebhookManager a(argc, argv);
-        switch(a.exec())
+        try
         {
-            case ohtoai::Finish:
-                return 0;
-            case ohtoai::Terminate:
-                return -1;
-            case ohtoai::Reload:
-            default:
-                break;
+            switch(a.exec())
+            {
+                case ohtoai::Finish:
+                    return 0;
+                case ohtoai::Terminate:
+                    return -1;
+                case ohtoai::Reload:
+                default:
+                    break;
+            }
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+            return -1;
         }
     }
 }
